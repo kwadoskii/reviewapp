a3d934bf4cf1a2cde84f38018e18b07e2404cedd
Austin Ofor <kwadoskii@live.com>
2021-10-05 12:56:58 +0100

    bumped expo to SDK 42 and AppLoading refactoring

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
-import Drawer from './routes/Drawer';
+import Drawer from "./routes/Drawer";
 
-const getFonts = () => Font.loadAsync({
-    'lucida': require('./assets/fonts/Lfax.ttf'),
-    'lucida-bold': require('./assets/fonts/Lfaxd.ttf')
-});
-
+const getFonts = () =>
+    Font.loadAsync({
+        lucida: require("./assets/fonts/Lfax.ttf"),
+        "lucida-bold": require("./assets/fonts/Lfaxd.ttf"),
+    });
 
 export default function App() {
     const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -26,7 +26,8 @@ export default function App() {
             <AppLoading
                 startAsync={getFonts}
                 onFinish={() => setFontsLoaded(true)}
+                onError={console.warn}
             />
-        )
+        );
     }
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
-import {} from "react-native";
 
 import { globalStyles, images } from "../styles/global";
 import Card from "../shared/Card";
